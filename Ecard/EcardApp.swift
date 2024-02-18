3ce417e186b79fe44eb312a2099e74adef3d2a7c
joseph <josephiacono5@gmail.com>
2024-02-18 17:33:32 -0500

    Initial Commit

---
+++ Ecard/EcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcardApp.swift
+//  Ecard
+//
+//  Created by Joseph Iacono on 2024-02-18.
+//
+
+import SwiftUI
+
+@main
+struct EcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
